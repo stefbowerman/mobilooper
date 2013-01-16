3fbe7c80cc109f118b0287495812c8b839cb399e
Stefan <stefan@ichange.com>
2013-01-15 18:02:18 -0800

    Initial commit of cordova project

---
+++ www/js/app.js
@@ -0,0 +1,250 @@
+//PhoneGap media constants 
+
+//Utils
+var lpr = window.lpr = {};
+
+	lpr.addRemoveFavorite = function(id, elem){
+
+		var myFavorites = store.get('favorites') ? store.get('favorites') : [];
+
+		if(_.contains(myFavorites, id)){
+			var newFavorites = _.reject(myFavorites, function(val){return val === id}); //remove the value and set it as out newFavorites variable
+			store.set('favorites', newFavorites);
+			elem.removeClass('favorite');
+		}
+		else{
+			myFavorites.push(id);
+			store.set('favorites', myFavorites);
+			elem.addClass('favorite');
+		}
+	}
+
+// Models
+
+Loop = Backbone.Model.extend({});
+
+LoopCollection = Backbone.Collection.extend({
+
+	model : Loop,
+
+	fetch : function(sort){
+		this.models = getLoopStore(sort);
+	}
+});
+
+// Views
+
+	// Main display view.  Takes care of the chrome.  LoopListView is only responsible for rendering the list
+MainLoopView = Backbone.View.extend({
+
+	initialize : function(){
+		this.template = _.template($('#main-loop').html());
+	},
+
+	render : function(){
+		$(this.el).html(this.template({options : this.options}));
+		this.listView = new LoopListView({el : $('ul', this.el), model : this.model});
+		this.listView.render();
+		return this;
+	}
+});
+
+LoopListView = Backbone.View.extend({
+	
+	initialize : function(){
+		this.model.bind("reset", this.render, this);
+	},
+
+	render : function(eventName){
+		$(this.el).empty();
+		if(!this.model.models.length){ //handle things if there's no data to be displayed
+			$(this.el).append('<li>nothing found</li>');
+		}
+		else{
+			_.each(this.model.models, function(loop){
+				$(this.el).append(new LoopListItemView({model : loop}).render().el);
+			}, this);
+		}
+		return this;
+	}
+});
+
+LoopListItemView = Backbone.View.extend({
+	tagName : 'li',
+
+	//attributes : {'class' : 'test-class'},
+
+	initialize : function(){
+		this.template = _.template($('#loop-list-item').html());
+	},
+
+	render : function(eventName){
+		$(this.el).html(this.template(this.model));
+		return this;
+	},
+
+	events : {
+		'click .star' : 'addRemoveFavorite',
+		'click .loop-list-item-holder' : 'goToLoop'
+	},
+
+	addRemoveFavorite : function(event){
+		if($(event.target).data('loop')){
+			var elFavorite = $(event.target),
+				loopId = $(event.target).data('loop');
+			lpr.addRemoveFavorite(loopId, elFavorite);
+
+		}
+	},
+
+	goToLoop : function(event){
+		var elem = $(event.target).closest('.loop-list-item-holder');
+		if(elem.data('loop')){
+			window.location.href = '#loop/' + elem.data('loop');
+		}
+	}
+
+});
+
+LoopView = Backbone.View.extend({
+
+	template : _.template($('#loop-detail').html()),
+
+	audio : '',
+
+	audio_name : "audio_1",
+
+	audio_playing : false,
+
+	initialize : function(){
+		this.audio = new Media(this.model.path);
+		this.audio.play( {numberOfLoops:-1} );
+		_.bindAll(this);
+	},
+
+	render : function(){
+		$(this.el).html(this.template(this.model));
+
+		return this;
+	},
+
+	events: {
+		"mousedown #play" : "playAudio",
+		"mousedown #stop" : "stopAudio",
+		"click .star" : "addRemoveFavorite",
+		"mousedown #back" : "goBack"
+	},
+
+	playAudio : function(){
+		if(this.audio){
+			this.audio.seekTo(0);
+			this.audio.play( {numberOfLoops:-1} );
+		}
+	},
+
+	stopAudio : function(){
+		if(this.audio){
+			this.audio.stop();
+			this.audio.seekTo(0);			
+		}
+	},
+
+	addRemoveFavorite : function(event){
+		if($(event.target).data('loop')){
+			var elFavorite = $(event.target),
+				loopId = $(event.target).data('loop');
+			lpr.addRemoveFavorite(loopId, elFavorite);
+
+		}
+	},
+
+	goBack : function(){
+		if(this.audio){
+			this.audio.stop();
+			this.audio.release();
+		}
+		window.history.back();
+	},
+});
+
+// Router
+var AppRouter = Backbone.Router.extend({
+	routes : {
+		'' : 'list',
+		'bpm' : 'bpm',
+		'favorites' : 'favorites',
+		'title' : 'title',
+		'loop/:id' : 'loopDetail'
+	},
+
+	initialize : function(){
+		var self = this;
+
+		// Check of browser supports touch events...
+        if ('ontouchstart' in document.documentElement) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('#content').on('touchstart', 'a', function(event){
+                self.selectItem(event);
+            });
+            $('#content').on('touchend', 'a', function(event){
+                self.deselectItem(event);
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('#content').on('mousedown', 'a', function(event){
+                self.selectItem(event);
+            });
+            $('#content').on('mouseup', 'a', function(event){
+                self.deselectItem(event);
+            });
+        }
+	},
+
+	selectItem:function(event) {
+        $(event.target).addClass('tappable-active');
+    },
+
+    deselectItem:function(event) {
+        $(event.target).removeClass('tappable-active');
+    },
+
+	createListing : function(sort){ //creates a list view for each of the different listing options
+		sort = (typeof sort === 'undefined') ? 'bpm' : sort;
+
+		if(!this.loopList){
+			this.loopList = new LoopCollection();
+		}
+		this.Main
+		this.MainLoopView = new MainLoopView({model : this.loopList, sortType : sort});
+		this.loopList.fetch(sort);
+		$('#content').html(this.MainLoopView.render().el);
+	},
+
+	list : function(){
+		this.createListing();
+	},
+
+	bpm : function(){
+		this.createListing('bpm');
+	},
+
+	favorites : function(){
+		this.createListing('favorites');
+	},
+
+	title : function(){
+		this.createListing('title');
+	},
+
+	loopDetail : function(id){
+		if(!this.loopList){
+			this.loopList = new LoopCollection();
+			this.loopList.fetch();
+		}
+
+		// TODO - Use getter
+		this.loop = _.find(this.loopList.models, function(model){return model.id == id ;})
+		this.loopView = new LoopView({model : this.loop});
+		$('#content').html(this.loopView.render().el);
+	}
+});
---
+++ www/js/loopStore.js
@@ -0,0 +1,144 @@
+var loopStore = [
+	{
+		id : 1, 
+		title : 'DirtStyle',
+		artist : '', 
+		bpm : 90, 
+		path : 'clips/clip.mp3', 
+		duration : 4290, 
+		img : 'img/dirtstyle.jpeg'
+	},
+
+	{
+		id : 2, 
+		title : 'Needle Thrashers Alpha 1',
+		artist : '', 
+		bpm : 93, 
+		path : 'clips/nt_alpha_1.mp3', 
+		duration : 2660, 
+		img : 'img/nt_alpha_1.jpg'
+	},
+
+	{
+		id : 3,
+		title : 'Trap Shit 7',
+		artist : 'UZ',
+		bpm : 70, 
+		path : 'clips/trap_shit_7.mp3',
+		duration : 13765,
+		img : 'img/trap_shit_7.jpg'
+	},
+
+	{
+		id : 4,
+		title : 'I See U (The Blessings Remix)',
+		artist : 'Lunice',
+		bpm : 60,
+		path : 'clips/lunice-I_see_U_blessings_remix.mp3',
+		duration : 8070,
+		img : 'img/lunice-I_see_U_blessings_remix.jpg',
+	},
+
+	{
+		id : 5,
+		title : 'UFO Beat',
+		artist : 'Toadstyle',
+		bpm : 84,
+		path : 'clips/toadstyle-ufo_beat.mp3',
+		duration : 11460,
+		img : 'img/toadstyle-get_em.jpg'
+	},
+
+	{
+		id : 6,
+		title : 'Horny Martian Breaks #2',
+		artist : 'DJ Flare',
+		bpm : 117,
+		path : 'clips/horny_martian_breaks_2.mp3',
+		duration : 4140,
+		img : 'img/horny_martian_breaks.jpg',
+	},
+
+	{
+		id : 7,
+		title : 'Super Duper Duck Breaks #1',
+		artist : 'DJ Babu',
+		bpm : 80, 
+		path : 'clips/super_duper_duck_1.mp3',
+		duration : 12040,
+		img : 'img/super_duper_duck.jpg'
+	},
+
+	{
+		id : 8,
+		title : 'Plastic',
+		artist : 'Doshy',
+		bpm : 90,
+		path : 'clips/plastic.mp3',
+		duration : 10700,
+		img : 'img/doshy_electrophilic.jpg'
+	},
+
+	{
+		id : 9,
+		title : 'Malt Flickr',
+		artist : 'HeRobust',
+		bpm : 70,
+		duration : 10700,
+		path : 'clips/herobust-malt_flickr.mp3',
+		img : 'img/herobust-malt_flickr.jpg',
+	},
+
+	{
+		id : 10,
+		title : 'Swag',
+		artist : 'David Banner',
+		bpm : 71,
+		duration : 10700,
+		path : 'clips/david_banner-swag.mp3',
+		img : 'img/david_banner-swag.jpg'
+	}
+]
+
+var getLoopStore = function(sort){
+	sort = (typeof sort === 'undefined') ? 'bpm' : sort;
+
+	if(sort === 'favorites'){
+		return favoriteSort(loopStore);
+	}
+	else if(sort === 'title'){
+		return titleSort(loopStore);
+	}
+	else{
+		return bpmSort(loopStore);		
+	}
+}
+
+var bpmSort = function(loopStore){
+
+	var sortedArray = _.sortBy(loopStore, function(loop){
+		return (-loop['bpm']);
+	});
+	return sortedArray;
+}
+
+var titleSort = function(loopStore){
+
+	var sortedArray = _.sortBy(loopStore, function(loop){
+		return loop['title'];
+	});
+
+	return sortedArray;
+}
+
+var favoriteSort = function(loopStore){
+
+	var favorites = store.get('favorites');
+
+	var sortedArray = _.filter(loopStore, function(loop){
+		return _.contains(favorites, loop.id);
+	});
+
+	//filter for favorites, then sort by BPM
+	return bpmSort(sortedArray);
+}
